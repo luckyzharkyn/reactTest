b5c8d595099b04783768c330006a76a2b102a13a
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-08 19:50:24 +0600

    кнопка для добавления элементов в массив

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
-	
-	return <div>
 
+	const [value, setValue] = useState([1, 2, 3, 4, 5]);
+
+	const result = value.map((elem, index) => {
+		return <span key={index}>{elem}</span>
+	})
+
+	function AddElem() {
+		setValue([...value, value.length + 1])
+	}
+
+	return <div>
+		{result}
+		<button onClick={AddElem}>add</button>
 	</div>
 }
 
