f2ffb0dad9bd2ce66623c6acbe6257ab6118b780
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-08 20:00:31 +0600

    изменение при клике

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 	const [value, setValue] = useState([1, 2, 3, 4, 5]);
 
 	const result = value.map((elem, index) => {
-		return <span key={index}>{elem} </span>
+		return <span key={index} onClick={() => {ChangeElem(index)}}>{elem} </span>
 	})
 
 	function AddElem() {
@@ -16,6 +16,10 @@ function App() {
 		let copy = Object.assign([], value);
 		copy.pop()
 		setValue(copy);
+	}
+
+	function ChangeElem(id) {
+		setValue([...value.slice(0, id), "!", ...value.slice(id + 1)])
 	}
 
 	return <div>
