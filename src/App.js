28d831e92aeaac9f8aafe37570881f84bd3582f0
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-08 19:56:40 +0600

    удаление из массива

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ function App() {
 	const [value, setValue] = useState([1, 2, 3, 4, 5]);
 
 	const result = value.map((elem, index) => {
-		return <span key={index}>{elem}</span>
+		return <span key={index}>{elem} </span>
 	})
 
 	function AddElem() {
 		setValue([...value, value.length + 1])
 	}
 
+	function RemoveLastElem() {
+		let copy = Object.assign([], value);
+		copy.pop()
+		setValue(copy);
+	}
+
 	return <div>
 		{result}
 		<button onClick={AddElem}>add</button>
+		<button onClick={RemoveLastElem}>delete</button>
 	</div>
 }
 
