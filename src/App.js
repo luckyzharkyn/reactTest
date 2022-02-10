6be549a25a4f49d0aa0a7c376f6bd336c9957540
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-10 10:54:33 +0600

    работа с объектами

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-	const [notes, setNotes] = useState(['a', 'b', 'c', 'd', 'e'])
-	const [editNum, setEditNum] = useState(null)
-	const [value, setValue] = useState("")
-
-	const result = notes.map((note, index) => {
-		return <li key={index} onClick={() => startEdit(index)}>{note}</li>
+	const [obj, setObj] = useState({
+		prop1: 'value1',
+		prop2: 'value2',
+		prop3: 'value3'
 	})
 
-	function startEdit(index) {
-		setEditNum(index);
-		setValue(notes[index])
-	}
-
-	function changeInput(event) {
-		setValue(event.target.value);
-
-		if(editNum) {
-			setNotes([...notes.slice(0, editNum), event.target.value, ...notes.slice(editNum + 1)])
-		}
-	}
-
-	function addItem(event) {
-		if(value != "") {
-			if(editNum) {
-				setEditNum(null)
-			} else {
-				setNotes([...notes, value])
-			}
-			setValue("")
-		}
+	function changeInput(prop, event) {
+		setObj({...obj, ...{[prop]: event.target.value}})
 	}
 
 	return <div>
-		{result}
-		<input value={value} onChange={(event) => changeInput(event)} onBlur={(event) => addItem(event)} />
+		<input value={obj.prop1} onChange={(event) => changeInput('prop1', event)} />
+		<input value={obj.prop2} onChange={(event) => changeInput('prop2', event)} />
+		<input value={obj.prop3} onChange={(event) => changeInput('prop3', event)} />
+
+		<br />
+
+		{obj.prop1} - {obj.prop2} - {obj.prop3}
 	</div>
 }
 
