b8ea108ce7d517d8d81baca867f2ad06da33dc85
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-11 21:24:19 +0600

    Добавленние объекта в массив объектов версия 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 
 function App() {
+	function getReadyObj() {
+		return {
+			id: 4,
+			name: '',
+			catg: '',
+			cost: ''
+		}
+	}
 
 	const initProds = [
 		{id: 1, name: 'prod1', catg: 'catg1', cost: 100},
@@ -8,12 +16,13 @@ function App() {
 		{id: 3, name: 'prod3', catg: 'catg3', cost: 300},
 	];
 	const [notes, setNotes] = useState(initProds);
+	const [obj, setObj] = useState(getReadyObj());
 	
 	const result = notes.map(note => {
 		return <tr key={note.id}>
-			<td>{note.name}</td>,
-			<td>{note.catg}</td>, 
-			<td>{note.cost}</td>,
+			<td>{note.name}</td>
+			<td>{note.catg}</td>
+			<td>{note.cost}</td>
 			<td><button onClick={(event) => remItem(note.id)}>delete this</button></td>
 		</tr>;
 	});
@@ -21,11 +30,22 @@ function App() {
 	function remItem(id) {
 		setNotes(notes.filter(note => note.id !== id))
 	}
-	
-	
+
+	function changeObj(propName, event) {
+		setObj({...obj, [propName]: event.target.value})
+	}
+
+	function addItem() {
+		setNotes([...notes, obj])
+	}
 	
 	return <div>
 		<table><tbody>{result}</tbody></table>
+
+		<input value={obj.name} onChange={(event) => changeObj('name', event)} />
+		<input value={obj.catg} onChange={(event) => changeObj('catg', event)} />
+		<input value={obj.cost} onChange={(event) => changeObj('cost', event)} />
+		<button onClick={addItem}>add new obj</button>
 	</div>;
 }
 
