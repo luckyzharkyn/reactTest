f8d608c7ab9a01faf1dad352d6f82ebf82539c3f
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-16 21:48:14 +0600

    при нажатии редактировать, отпустить и сохранить стейт

--- src/App.js
+++ src/App.js
@@ -1,75 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
-	const initProds = [
-		{
-			id: 1,
-			name: 'prod1',
-			cost: 'cost1',
-			desc: 'long description 1',
-			comm: 'my super comment 1',
-			descShow: false,
-			commShow: false
-		},
-		{
-			id: 2,
-			name: 'prod2',
-			cost: 'cost2',
-			desc: 'long description 2',
-			comm: 'my super comment 2',
-			descShow: false,
-			commShow: false
-		},
-		{
-			id: 3,
-			name: 'prod3',
-			cost: 'cost3',
-			desc: 'long description 3',
-			comm: 'my super comment 3',
-			descShow: false,
-			commShow: false
-		},
-	];
+	const [value, setValue] = useState("text");
+	const [isEdit, setIsEdit] = useState(false);
 
-	const [notes, setNotes] = useState(initProds);
-
-	const result = notes.map(note => {
-		let descShow, commShow;
-		if(note.descShow) {
-			descShow = ", " + note.desc;
-		}
-		if(note.commShow) {
-			commShow = ", " + note.comm;
-		}
-
-
-		return <li key={note.id}>{note.name}, {note.cost} {descShow} {commShow} 
-		<button onClick={() => showDesc(note.id)}>{note.descShow ? "hideDesc" : "showDesc"}</button>
-		<button onClick={() => showComm(note.id)}>{note.commShow ? "hideComm" : "showComm"}</button>
-		</li>
-	})
-
-	function showDesc(id) {
-		setNotes(notes.map(note => {
-			if(note.id === id) {
-				return {...note, descShow: !note.descShow}
-			} else {
-				return note;
-			}
-		}))
+	let elem;
+	if(!isEdit) {
+		elem = <p>{value}</p>
+	} else {
+		elem = <input value={value} onChange={(event) => setValue(event.target.value)}/>
 	}
 
-	function showComm(id) {
-		setNotes(notes.map(note => {
-			if(note.id === id) {
-				return {...note, commShow: !note.commShow}
-			} else {
-				return note;
-			}
-		}))
-	}
 	return <div>
-		<ul>{result}</ul>
+		{elem}
+		<button onClick={() => setIsEdit(true)}>Edit</button>
+		<button onClick={() => setIsEdit(false)}>Save</button>
 	</div>
 }
 
