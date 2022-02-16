c984716f4fe26a9c573e27cc9576c27534cb870d
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-16 22:34:32 +0600

    Несколько список изменить по нажатию

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import React, { useState } from "react";
 
 function App() {
-	const [value, setValue] = useState("text");
-	const [isEdit, setIsEdit] = useState(false);
+	const initNotes = [
+		{	id: 1,
+			text: 'note1',
+			isEdit: false
+		},
+		{	id: 2,
+			text: 'note2',
+			isEdit: false
+		},
+		{	id: 3,
+			text: 'note3',
+			isEdit: false
+		}
+	]; 
 
-	let elem;
-	if(!isEdit) {
-		elem = <p>{value}</p>
-	} else {
-		elem = <input value={value} onChange={(event) => setValue(event.target.value)}/>
+	const [notes, setNotes] = useState(initNotes);
+
+	const result = notes.map(note => {
+		let elem;
+
+		if(!note.isEdit) {
+			elem = note.text;
+		} else {
+			elem = <input value={note.text} onChange={(event) => changeText(note.id, event)}/>
+		}
+
+		return <li key={note.id}>{elem}
+		<button onClick={() => startChoose(note.id, note.isEdit)}>{note.isEdit ? "save" : "change"}</button>
+		</li>
+	});
+
+	function changeText(id, event) {
+		changeIsEdit(id, event.target.value, 'text')
+	}
+
+	function startChoose(id, isEdit) {
+		if(!isEdit) {
+			changeIsEdit(id, true);
+		} else {
+			changeIsEdit(id, false);
+		}
+	}
+
+	function changeIsEdit(id, value, key='isEdit') {
+		setNotes(notes.map(note => {
+			if(note.id === id) {
+				return {...note, [key]: value}
+			} else {
+				return note;
+			}
+		}))
 	}
 
 	return <div>
-		{elem}
-		<button onClick={() => setIsEdit(true)}>Edit</button>
-		<button onClick={() => setIsEdit(false)}>Save</button>
+		<ul>{result}</ul>
 	</div>
 }
 
