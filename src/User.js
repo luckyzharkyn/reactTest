c9c9832974adf5241a316fa1feaa7446edfc69fa
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-17 10:16:53 +0600

    Редактирование стейта родителя в дочернем компоненте

--- src/User.js
+++ src/User.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-function User({id, name, surname, age, ban, addBlock}) {
+function User({id, name, surname, age, ban, isEdit, addBlock, toggleMode, editProd}) {
     return <div>
         <span>{id} </span>
-        <span>{name} </span>
-        <span>{surname} </span>
-        <span>{age} </span>
+
+        {isEdit
+        ? <input value={name} onChange={(event) => editProd(id, "name", event)}/>
+        : <span>{name} </span>}
+
+        {isEdit
+        ? <input value={surname} onChange={(event) => editProd(id, "surname", event)}/>
+        : <span>{surname} </span>}
+
+        {isEdit
+        ? <input value={age} onChange={(event) => editProd(id, "age", event)}/>
+        : <span>{age} </span>}
+
         <span>status: {ban ? "ban" : "free"}</span>
         <button onClick={() => addBlock(id)}>{ban ? "unBlock" : "block"}</button>
+        <button onClick={() => toggleMode(id)}>{isEdit ? "save" : "edit"}</button>
+
     </div>
 }
 
--- src/Users.js
+++ src/Users.js
@@ -1,11 +1,12 @@
+import { func } from "prop-types";
 import React, { useState } from "react";
 import User from "./User";
 
 function Users() {
     const initUsers = [
-        {id: 1, name: 'user1', surname: 'surn1', age: 30, ban: false},
-        {id: 2, name: 'user2', surname: 'surn2', age: 31, ban: false},
-        {id: 3, name: 'user3', surname: 'surn3', age: 32, ban: false},
+        {id: 1, name: 'user1', surname: 'surn1', age: 30, ban: false, isEdit: false},
+        {id: 2, name: 'user2', surname: 'surn2', age: 31, ban: false, isEdit: false},
+        {id: 3, name: 'user3', surname: 'surn3', age: 32, ban: false, isEdit: false},
     ];
 
     const [users, setUsers] = useState(initUsers);
@@ -17,9 +18,33 @@ function Users() {
                     surname={user.surname}
                     age={user.age}
                     ban={user.ban}
+                    isEdit={user.isEdit}
                     addBlock={addBlock}
+                    toggleMode={toggleMode}
+                    editProd={editProd}
                 />
     })
+
+
+    function toggleMode(id) {
+        setUsers(users.map(user => {
+            if(user.id === id) {
+                user.isEdit = !user.isEdit;
+            }
+
+            return user;
+        }))
+    }
+
+    function editProd(id, field, event) {
+        setUsers(users.map(user => {
+            if(user.id === id) {
+                user[field] = event.target.value;
+            }
+
+            return user;
+        }))
+    }
 
     function addBlock(id) {
         setUsers(users.map(user => {
